6f0e31cb113d20afd19fd55726f5b0b9d49dc146
xhnbzdl <2804184253@qq.com>
2022-04-09 01:07:39 +0800

    添加项目文件。

---
+++ Tencent.OSS.ConsoleApp.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tencent.OSS.ConsoleApp.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
