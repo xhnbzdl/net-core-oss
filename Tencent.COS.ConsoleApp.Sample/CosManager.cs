a4e12e01c792dc2fad79ab4ba6e5651d779df6d4
xhnbzdl <2804184253@qq.com>
2022-04-10 14:00:55 +0800

    更改目录文件命名，腾讯COS

---
+++ Tencent.COS.ConsoleApp.Sample/CosManager.cs
@@ -0,0 +1,23 @@
+﻿using COSXML;
+
+namespace Tencent.COS.ConsoleApp.Sample
+{
+    public class CosManager
+    {
+        public static void InitTencentCos()
+        {
+            // 腾讯云 SecretId
+            string secretId = "AKIDUnrgGbgW6RWMwIOFqCJAm8E4f05vVpuj";
+            // 腾讯云 SecretKey
+            string secretKey = "bw2siJflcFdmfIgohKhusb4HExQhjVWI";
+            // 存储桶所在地域
+            string region = "ap-nanjing";
+
+            // 普通初始化方式
+            CosXmlConfig config = new CosXmlConfig.Builder()
+                .SetRegion(region)
+                .SetDebugLog(true)
+                .Build();
+        }
+    }
+}
---
+++ Tencent.COS.ConsoleApp.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tencent.COS.ConsoleApp.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // 获取系统环境变量
+            string secretId = Environment.GetEnvironmentVariable("SECRET_ID");
+            Console.WriteLine(secretId);
+        }
+    }
+}
